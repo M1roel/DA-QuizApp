7d3ffa9cddd7d86a45d1058a4626f2e28a5f847d
M1roel <157500795+M1roel@users.noreply.github.com>
2024-06-24 15:20:46 +0200

    create functions in script.js - init() - initialise the question card - showQuestion() - dynamic input question and answers from the JSON into the question card

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 let questions = [
     {
-        "question": "Wer hat HTML erfunden",
+        "question": "Wer hat HTML erfunden?",
         "answer_1": "Robbie Williams",
         "answer_2": "Lady Gaga",
         "answer_3": "Tim Berners-Lee",
@@ -80,3 +80,19 @@ let questions = [
         "right_answer": 3
     }
 ];
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('total_questions').innerText = questions.length;
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('questiontext').innerHTML = question.question;
+    document.getElementById('answer_1').innerHTML = question.answer_1;
+    document.getElementById('answer_2').innerHTML = question.answer_2;
+    document.getElementById('answer_3').innerHTML = question.answer_3;
+    document.getElementById('answer_4').innerHTML = question.answer_4;
+}
